daf2b8c9d9761fd99e21f9e94d1ac1eb905fe20d
David Urbancok <urbancok.david@outlook.com>
2021-01-02 21:03:48 +0100

    Add Day 2, Challenge 1

---
+++ December2/PasswordInput.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace December2
+{
+    public class PasswordInput
+    {
+        public char Character { get; }
+
+        public Range CharacterRange { get; }
+
+        public string Password { get; }
+
+        public bool Valid { get; private set; } = false;
+
+        public PasswordInput(string line)
+        {
+            var segments = line.Split(' ');
+            Character = segments[1][0];
+            Password = segments[2];
+
+            var ranges = segments[0].Split('-');
+            CharacterRange = new Range(Convert.ToInt32(ranges[0]), Convert.ToInt32(ranges[1]));
+        }
+
+        public void Validate()
+        {
+            var charOccurences = Password.ToList().Count(c => c == Character);
+
+            Valid = CharacterRange.Start.Value <= charOccurences
+                 && CharacterRange.End.Value >= charOccurences;
+        }
+    }
+}
---
+++ December2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace December2
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            using var fileStream = new FileStream("data.txt", FileMode.Open, FileAccess.Read);
+            using var streamReader = new StreamReader(fileStream);
+
+            var lines = streamReader.ReadToEnd().Split('\n');
+            var passwords = new List<PasswordInput>();
+
+            foreach (var line in lines)
+            {
+                PasswordInput password = new PasswordInput(line);
+                passwords.Add(password);
+                password.Validate();
+            }
+
+            Console.WriteLine($"Valid passwords: {passwords.Count(p => p.Valid)}");
+        }
+    }
+}
